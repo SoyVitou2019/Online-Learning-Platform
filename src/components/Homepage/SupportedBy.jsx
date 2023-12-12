df84209abdac23152a81607692a3064833fbb65d
Eong-Koungmeng <107832453+Eong-Koungmeng@users.noreply.github.com>
2023-12-12 18:25:20 +0700

    Add supported by, and footer

---
+++ src/components/Homepage/SupportedBy.jsx
@@ -0,0 +1,16 @@
+export const SupportedBy = () => {
+  return (
+    <div className="text-gray-900">
+      <h1 className="text-center py-16 text-4xl font-bold tracking-tight leading-none dark:text-dark">
+        Supported By
+      </h1>
+
+      <div className=" flex flex-wrap justify-center space-x-20 pb-10">
+        <img src="https://fakeimg.pl/100x100" alt="" />
+        <img src="https://fakeimg.pl/100x100" alt="" />
+        <img src="https://fakeimg.pl/100x100" alt="" />
+        <img src="https://fakeimg.pl/100x100" alt="" />
+      </div>
+    </div>
+  );
+};
--- src/components/Pages/LandingPage.jsx
+++ src/components/Pages/LandingPage.jsx
@@ -3,6 +3,9 @@ import { HeroSection } from "../HomePage/HeroSection";
 import { PopularCoursesSection } from "../HomePage/PopularCoursesSection";
 import { HowVMWorks } from "../HomePage/HowVMWorks";
 import { Testamonial } from "../HomePage/Testamonial";
+import { AboutUs } from "../Homepage/AboutUs";
+import { SupportedBy } from "../Homepage/SupportedBy";
+import { Footer } from "../Homepage/Footer";
 
 export const LandingPage = () => {
   return (
@@ -12,6 +15,9 @@ export const LandingPage = () => {
       <PopularCoursesSection />
       <HowVMWorks />
       <Testamonial />
+      <AboutUs />
+      <SupportedBy />
+      <Footer />
     </>
   );
 };
