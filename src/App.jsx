11b3e7ae42acf89b657608218ff1f9795d3b9e0b
Eong-Koungmeng <107832453+Eong-Koungmeng@users.noreply.github.com>
2023-12-12 20:40:44 +0700

    Add cardlandscape

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 //import { NavBar } from "./components/HomePage/NavBar";
 //import { HeroSection } from "./components/HomePage/HeroSection";
 //import { CardPortrait } from "./components/HomePage/CardPortrait";
-import { LandingPage } from "./components/Pages/LandingPage";
+//import { LandingPage } from "./components/Pages/LandingPage";
+
+import { CardLandscape } from "./components/Homepage/CardLandscape";
 
 function App() {
-  return <LandingPage />;
+  return <CardLandscape />;
 }
 
 export default App;
