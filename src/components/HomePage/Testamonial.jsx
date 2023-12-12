2cc44619057ae58a5f47b406b79e4f0a9f4dfc41
Eong-Koungmeng <107832453+Eong-Koungmeng@users.noreply.github.com>
2023-12-12 16:39:19 +0700

    Finish draft testimonial

--- src/components/HomePage/Testamonial.jsx
+++ src/components/HomePage/Testamonial.jsx
@@ -1,9 +1,18 @@
 export const Testamonial = () => {
   return (
-    <div>
-      <h1 className="text-center py-16 text-4xl font-bold tracking-tight leading-none dark:text-dark">
-        “I love using VM in my free time because it helps me learn more.”
+    <div className="p-12">
+      <h1 className="leading-relaxed text-center pb-8 text-4xl font-bold tracking-tight dark:text-dark">
+        “I love using VM in my free time because <br />
+        it helps me learn more.”
       </h1>
+
+      <div className="flex justify-end pr-52 items-center">
+        <p className="mr-5 text-xl font-bold tracking-tight text-gray-90 ">
+          Peakmi <br />
+          Takeo
+        </p>
+        <img src="https://fakeimg.pl/70x70" alt="" />
+      </div>
     </div>
   );
 };
