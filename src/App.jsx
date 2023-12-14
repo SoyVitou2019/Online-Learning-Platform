fdabd8ed3910a066a2eb4bed6a806f90f4c1ffd6
Eong-Koungmeng <107832453+Eong-Koungmeng@users.noreply.github.com>
2023-12-15 00:28:54 +0700

    Profile page
    Banner, profile, navbar profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 //import { NavBar } from "./components/HomePage/NavBar";
 //import { HeroSection } from "./components/HomePage/HeroSection";
 //import { CardPortrait } from "./components/HomePage/CardPortrait";
-import { LandingPage } from "./components/Pages/LandingPage";
+//import { LandingPage } from "./components/Pages/LandingPage";
+import { ProfilePage } from "./components/Pages/ProfilePage";
 
 //import { CardLandscape } from "./components/Homepage/CardLandscape";
 
 function App() {
-  return (
-    <Test />
-  )
+  return <ProfilePage />;
 }
 
 export default App;
---
+++ src/components/Pages/ProfilePage.jsx
@@ -0,0 +1,45 @@
+export const ProfilePage = () => {
+  return (
+    <>
+      <div className="relative">
+        <img
+          src="https://fakeimg.pl/1600x900"
+          alt="Banner Image"
+          className="w-full h-64 object-cover"
+        />
+      </div>
+      <div className="flex items-center justify-between pl-8 text-black -translate-y-6">
+        <div className="flex items-center">
+          <img
+            src="https://fakeimg.pl/100x100"
+            alt="Profile Image"
+            className="w-30 h-30 rounded-full mr-2"
+          />
+          <div className="pl-5">
+            <h3 className="text-xl">Student</h3>
+            <h2 className="text-xl font-semibold">Eong Koungmeng</h2>
+            <div className="flex justify-start">
+              <a href="#" className=" py-2 text-sm font-medium text-center">
+                Follower: 2
+              </a>
+              <a href="#" className="ml-auto py-2 text-sm font-medium ">
+                Following: 4
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pl-5 flex justify-start border-t border-b border-gray-300">
+        <a href="#" className=" p-4 py-2 text-sm font-medium text-center">
+          About
+        </a>
+        <a href="#" className=" p-4  py-2 text-sm font-medium ">
+          Learning
+        </a>
+        <a href="#" className=" p-4  py-2 text-sm font-medium ">
+          Contact
+        </a>
+      </div>
+    </>
+  );
+};
