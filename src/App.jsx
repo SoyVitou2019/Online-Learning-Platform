9560f7e71deb30822bc86101805e6d27a5322586
soyvitou2019 <soyvitou999@gmail.com>
2023-12-23 11:48:58 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 //import { NavBar } from "./components/HomePage/NavBar";
 //import { HeroSection } from "./components/HomePage/HeroSection";
 //import { CardPortrait } from "./components/HomePage/CardPortrait";
-import { LandingPage } from "./components/Pages/LandingPage";
-
+// import { LandingPage } from "./components/Pages/LandingPage";
 //import { CardLandscape } from "./components/Homepage/CardLandscape";
+// import SelectCourse from "./components/Homepage/selectCourse";
+// import PlayCourse from "./components/Homepage/PlayCourse";
+import AdminPage from "./components/Homepage/AdminPage";
+
+import React from "react";
 
 function App() {
   return (
-    <Test />
+    <AdminPage />
   )
 }
 
---
+++ src/components/HomePage/HorizontalLine.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HorizontalLine = () => {
+  return (
+    <div style={{ borderTop: '1px solid #ddd', margin: '16px 0' }}></div>
+  );
+};
+
+export default HorizontalLine;
