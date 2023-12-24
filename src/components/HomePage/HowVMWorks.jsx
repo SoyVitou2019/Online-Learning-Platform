ed82c54be5349cb47e2f688d0360070d2b03cb01
lusamreth <somrethoverkill@gmail.com>
2023-12-24 17:29:10 +0700

    (fix) extract to global component

--- src/components/HomePage/HowVMWorks.jsx
+++ src/components/HomePage/HowVMWorks.jsx
@@ -1,17 +1,17 @@
 import { BenefitCard } from "./BenefitCard";
 
 export const HowVMWorks = () => {
-  return (
-    <div className="bg-gray-900 text-white">
-      <h1 className="text-center py-16 text-4xl font-bold tracking-tight leading-none dark:text-dark">
-        How VM Works?
-      </h1>
+    return (
+        <div className="bg-gray-900 text-white">
+            <h1 className="text-center py-16 text-4xl font-bold tracking-tight leading-none dark:text-dark">
+                How VM Works?
+            </h1>
 
-      <div className=" flex flex-wrap justify-center space-x-20 pb-10">
-        <BenefitCard />
-        <BenefitCard />
-        <BenefitCard />
-      </div>
-    </div>
-  );
+            <div className=" flex flex-wrap justify-center space-x-20 pb-10">
+                <BenefitCard />
+                <BenefitCard />
+                <BenefitCard />
+            </div>
+        </div>
+    );
 };
