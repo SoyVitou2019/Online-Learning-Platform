23aa308e96d233f34a611946d58fc6a32c03cd29
Eong-Koungmeng <107832453+Eong-Koungmeng@users.noreply.github.com>
2023-12-25 17:06:12 +0700

    landing page route

--- src/routes/public.jsx
+++ src/routes/public.jsx
@@ -1,7 +1,12 @@
 import { HomeRoutes } from "../features/homepage/route";
+import { LandingPage } from "../features/landing/route/LandingPage";
 import { ProfileRoutes } from "../features/profile/route";
 
 export const publicRoutes = [
+  {
+    path: "/landing",
+    element: <LandingPage />,
+  },
   {
     path: "/*",
     element: <HomeRoutes />,
