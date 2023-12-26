1c126b6b5cef2391153f8e893cba67e5c00d1f55
Koungmeng <107832453+Eong-Koungmeng@users.noreply.github.com>
2023-12-26 16:58:41 +0700

    Revert "Merge remote-tracking branch 'origin/vitou' into meng2"
    This reverts commit e543f8beb0a9903cb824133111a7e422bd3177e1, reversing
    changes made to 62614b86e3ec0790db0997233b0e7a0119803d50.

---
+++ src/components/LoginPage/ForgotPassword.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+export const Forgotpassword = () => {
+    return (
+        <form className = "login-style">
+            <p className="border-button text-center p-5">Forggot your password?</p>
+            <label htmlFor="email"></label>
+            <input className="border-button mt-4 pl-[5px] mr-10 ml-10 outline-none"  type="email" id="email" placeholder="Youremailaddress@gmail.com"/>
+            <p className="flex mr-6 ml-6 mt-6 mb-2 text-justify">If you have forgotten your password, please
+                 enter your account&apos; email address below and
+                  click the &quot;Reset My Password&quot; button. You will
+                   receive an email that contains a link that 
+                   contains a link to set a new password.</p>
+            <div className="flex mt-1 mb-3 justify-center">
+                <p>Already login?</p>
+                <Link to="/login" className="text-green-600 font-bold ml-1 hover:text-green-300">Back</Link>
+            </div>
+            <Link to="/changepassword" className="border-2 border-red-700 rounded-lg p-2 mr-7 ml-7 mb-9 text-center hover:bg-red-300">Reset my password</Link>
+        </form>
+    )
+}
--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -4,42 +4,42 @@ import { NavBar } from "../HomePage/NavBar";
 import SideBar from "../HomePage/SideBar";
 
 export const HomePage = () => {
-  return (
-    <>
-      <div className="mb-3">
-        <NavBar />
-      </div>
+    return (
+        <>
+            <div className="mb-3">
+                <NavBar />
+            </div>
 
-      <div className="flex justify-stretch">
-        <SideBar />
+            <div className="flex justify-stretch">
+                <SideBar />
 
-        <div className="mx-5 w-full">
-          <HomePageFilter />
-          <h1 className=" text-2xl font-bold m-2">Popular</h1>
-          <hr />
-          <div className="grid grid-cols-3 p-5 gap-4">
-            <CardPortrait />
-            <CardPortrait />
-            <CardPortrait />
+                <div className="mx-5 w-full">
+                    <HomePageFilter />
+                    <h1 className=" text-2xl font-bold m-2">Popular</h1>
+                    <hr />
+                    <div className="grid grid-cols-3 p-5 gap-4">
+                        <CardPortrait />
+                        <CardPortrait />
+                        <CardPortrait />
 
-            <CardPortrait />
+                        <CardPortrait />
 
-            <CardPortrait />
+                        <CardPortrait />
 
-            <CardPortrait />
-          </div>
-          <h1 className=" text-2xl font-bold m-2">News</h1>
-          <hr />
-          <div className="grid grid-cols-3 p-5 gap-4">
-            <CardPortrait />
-            <CardPortrait />
-            <CardPortrait />
-            <CardPortrait />
-            <CardPortrait />
-            <CardPortrait />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                        <CardPortrait />
+                    </div>
+                    <h1 className=" text-2xl font-bold m-2">News</h1>
+                    <hr />
+                    <div className="grid grid-cols-3 p-5 gap-4">
+                        <CardPortrait />
+                        <CardPortrait />
+                        <CardPortrait />
+                        <CardPortrait />
+                        <CardPortrait />
+                        <CardPortrait />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
