f48b644021dd2eace63ca65b6daa5585d315e3d9
Soy Vitou Pro <118577815+SoyVitou2019@users.noreply.github.com>
2023-12-31 00:19:42 +0700

    finish teach request

--- src/features/teach/components/Request.jsx
+++ src/features/teach/components/Request.jsx
@@ -2,15 +2,35 @@ import { Disclosure } from '@headlessui/react'
 import { ChevronUpIcon } from '@heroicons/react/20/solid'
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState, useEffect } from "react";
+import END_POINTS from '../../../constants/endpoints';
+import axios from 'axios';
 
 export const Request = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    user_id: 1,
+    phoneNumber: "",
+    reasonRequest: ""
+  });
   function openRequestModal() {
+    setFormData({
+      user_id: 1,
+      phoneNumber: "",
+      reasonRequest: ""
+    })
     setIsOpen(true);
   }
 
   function closeRequestModal() {
     setIsOpen(false);
+  }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  }
+  const handleSubmit = async () => {
+    closeRequestModal()
+    await axios.post(END_POINTS.USER_REQUEST, formData)
   }
   const role = [
     {
@@ -69,13 +89,14 @@ export const Request = () => {
       <hr className="my-4" />
       <div onClick={openRequestModal} className="flex justify-center select-none">
         <button type="button" className="px-14 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          <svg className="w-3 h-3 text-white me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+          <svg className="w-7 h-3 text-white me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
             <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
             <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
           </svg>
           Send Mail
         </button>
       </div>
+      <hr className="my-4" />
       {/* dialog */}
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeRequestModal}>
@@ -109,13 +130,44 @@ export const Request = () => {
                   >
                     Do you want to be a teacher for upload video?
                   </Dialog.Title>
-                  <div className="mt-2">
+
+                  <form className="max-w-sm mx-auto mt-5">
+                    <label htmlFor="user_id" className="block mb-2 text-sm font-medium text-black">User ID Request</label>
+                    <div className="relative">
+                      <div className="absolute inset-y-0 start-0 top-0 flex items-center ps-3.5 pointer-events-none">
+                        ID
+                      </div>
+                      <input type="text" id="user_id" value={formData.user_id} name='user_id' onChange={handleChange} aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full ps-10 p-2.5  " pattern="[0-9]{1}" placeholder="1" required />
+                    </div>
+                    <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400">currently is just dummy website.</p>
+                  </form>
+
+
+                  <form className="max-w-sm mx-auto mt-5">
+                    <label htmlFor="phone-input" className="block mb-2 text-sm font-medium text-black">Phone number</label>
+                    <div className="relative">
+                      <div className="absolute inset-y-0 start-0 top-0 flex items-center ps-3.5 pointer-events-none">
+                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
+                          <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z" />
+                        </svg>
+                      </div>
+                      <input type="text" id="phone-input" value={formData.phoneNumber} name='phoneNumber' onChange={handleChange} aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full ps-10 p-2.5  " pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="123-456-7890" required />
+                    </div>
+                    <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400">Select a phone number that matches the format.</p>
+                  </form>
+
+                  <div className="mt-5">
+                    <label htmlFor="reasonRequest" className="block mb-2 text-sm font-medium text-black">Write your reason here</label>
                     <textarea
-                      id="message"
+                      id="reasonRequest"
+                      name='reasonRequest'
+                      value={formData.reasonRequest}
+                      onChange={handleChange}
                       rows="4"
                       className="block p-2.5 w-full text-sm text-gray-800 bg-gray-50 rounded-lg"
                       placeholder="Write your thoughts here..."
                     ></textarea>
+                    <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400">The reason should be related to your experience.</p>
                   </div>
 
                   <div className="mt-4 flex justify-end gap-3">
@@ -129,7 +181,7 @@ export const Request = () => {
                     <button
                       type="button"
                       className="flex justify-end rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeRequestModal}
+                      onClick={handleSubmit}
                     >
                       Submit to be teacher
                     </button>
