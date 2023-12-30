7a16cd1225688986752f3ad1159bfa0b3ff68eae
Eong-Koungmeng <107832453+Eong-Koungmeng@users.noreply.github.com>
2023-12-31 00:01:24 +0700

    Revert "Merge branch 'meng2' of https://github.com/SoyVitou2019/Online-Learning-Platform into meng2"
    This reverts commit 30291d825fc6809f5563441016fb497d296555b3, reversing
    changes made to eee207155b530a15b1c500239a7efd27e9cd1747.

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,19 +1,18 @@
-import SideBar from "../components/SideBar";
-import { NavBar } from "../components/NavBar";
+import { NavBar } from "../components/HomePage/NavBar";
+import SideBar from "../components/HomePage/SideBar";
 
 export const HomeLayout = ({ children }) => {
-    return (
-        <>
-            <div>
-                <NavBar />
-            </div>
+  return (
+    <>
+      <div className="flex flex-col h-screen">
+        <NavBar />
 
-            <div className="flex justify-start">
-                <div className="sticky">
-                    <SideBar />
-                </div>
-                <div className="mx-5 w-full">{children}</div>
-            </div>
-        </>
-    );
+        <div className="flex items-stretch overflow-hidden h-full">
+          <SideBar />
+
+          <div className="px-5 w-full overflow-y-auto">{children}</div>
+        </div>
+      </div>
+    </>
+  );
 };
