ec2a18efd4526101ab6d5f252bc2f5b5d2469bd0
Eong-Koungmeng <107832453+Eong-Koungmeng@users.noreply.github.com>
2024-01-02 14:16:52 +0700

    update select course

--- src/constants/endpoints.jsx
+++ src/constants/endpoints.jsx
@@ -3,6 +3,9 @@ const END_POINTS = {
   USER_SORT: "http://localhost:3334/user?_sort=id&_order=asc",
   USER_REQUEST: "http://localhost:3334/user_request/",
   COURSE: "http://localhost:3334/course",
+  CATEGORY: "http://localhost:3334/category",
   KEY: "http://localhost:3334/service_role",
+  YT: "http://localhost:3334/youtube_key",
+  POST: "http://localhost:3334/post",
 };
 export default END_POINTS;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -11,8 +11,6 @@ export const AppRoutes = () => {
 
   const { user, role } = useAuth();
 
-  console.log(user.id);
-
   let routes;
 
   console.log(role);
