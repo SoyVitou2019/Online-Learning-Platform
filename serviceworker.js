306b4c901d91fdb54327a9adfa8a4daf8e8e6ab0
Soy Vitou Pro <118577815+SoyVitou2019@users.noreply.github.com>
2024-01-04 17:58:42 +0700

    change offline1

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = ["index.html", "./offline/offline.html"]
+const urlsToCache = ["index.html", "offline.html"]
 const self = this;
 
 
@@ -21,23 +21,23 @@ self.addEventListener('fetch', (event) =>{
         caches.match(event.request)
         .then(()=>{
             return fetch(event.request)
-            .catch(()=> caches.match('./offline/offline.html'))
+            .catch(()=> caches.match('offline.html'))
         })
     )
 });
 
 
 // Activate the SW
-self.addEventListener('activate', (event) =>{
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
-    event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) =>{
-                if(!cacheWhitelist.includes(cacheName)){
-                    return caches.delete(cacheName)
-                }
-            })
-        ))
-    )
-});
+// self.addEventListener('activate', (event) =>{
+//     const cacheWhitelist = [];
+//     cacheWhitelist.push(CACHE_NAME);
+//     event.waitUntil(
+//         caches.keys().then((cacheNames) => Promise.all(
+//             cacheNames.map((cacheName) =>{
+//                 if(!cacheWhitelist.includes(cacheName)){
+//                     return caches.delete(cacheName)
+//                 }
+//             })
+//         ))
+//     )
+// });
